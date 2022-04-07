01cfbed1a69b131009999ede1e32b012a567b5f0
Minhang Xie <minxie97@gmail.com>
2022-04-06 20:37:21 -0700

    lab complete plus component files

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+      <footer className="flex items-center justify-between px-5 py-3 bg-emerald-500">
+        <p className="text-sm text-center">@2022</p>
+      </footer>
+    )
+  }
---
+++ components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+    return(
+      <header className="flex items-center justify-between px-5 py-4 text-3xl bg-emerald-500">
+        <h1>Cookie Stand Admin</h1>
+      </header>
+    )
+  }
